6cebcef991058e96ba77d2179dcf37b5fa06e0cc
HALLOVFEM-MD <faisalmwombeki@gmail.com>
2025-03-24 10:29:09 +0300

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -34,7 +34,7 @@ const date = moment().format('DD/MM/YYYY');
 
   let infoMsg =  `
 ╭──────────────────❂
-┊❂╭───*𝐀𝐍𝐘𝐖𝐀𝐘-𝐗𝐌𝐃*────❂
+┊❂╭───*𝐇𝐀𝐋𝐋𝐎𝐕𝐅𝐄𝐌-𝐗𝐌𝐃*────❂
 ┊✺┊ *User* : ${s.OWNER_NAME}
 ┊✺┊ *Mode* : ${mode}
 ┊✺╰───────────────❂
@@ -44,7 +44,7 @@ const date = moment().format('DD/MM/YYYY');
 ╰──────────────────❂ \n\n`;
  
     let menuMsg=`  
-  **𝐀𝐍𝐘𝐖𝐀𝐘-𝐗𝐌𝐃 COMMANDS*
+  **𝐇𝐀𝐋𝐋𝐎𝐕𝐅𝐄𝐌-𝐗𝐌𝐃 COMMANDS*
 `;
 
     for (const cat in coms) {
@@ -61,7 +61,7 @@ const date = moment().format('DD/MM/YYYY');
 ◇            ◇
 *—————✺✺✺✺—————*
 
-  *𝐀𝐍𝐘𝐖𝐀𝐘-𝐗𝐌𝐃*                                         
+  *𝐇𝐀𝐋𝐋𝐎𝐕𝐅𝐄𝐌-𝐗𝐌𝐃*                                         
 *╰═════════════❂*
 `;
 
@@ -72,8 +72,8 @@ const date = moment().format('DD/MM/YYYY');
         zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Zokou-MD*, développé par Djalega++" , gifPlayback : true }, { quoted: ms });
     }
     catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
+        console.log("😁✌ Menu erreur " + e);
+        repondre("😁✌ Menu erreur " + e);
     }
 } 
 // Vérification pour .jpeg ou .png
@@ -82,8 +82,8 @@ else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
         zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "*Ibrahim-tech*" }, { quoted: ms });
     }
     catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
+        console.log("😁✌ Menu erreur " + e);
+        repondre("😁✌ Menu erreur " + e);
     }
 } 
 else {

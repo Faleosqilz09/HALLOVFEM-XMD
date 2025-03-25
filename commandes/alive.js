eb218b446d2359c29b4ebd6e191afa54d28ff248
HALLOVFEM-MD <faisalmwombeki@gmail.com>
2025-03-25 05:48:07 +0300

    Update alive.js

--- commandes/alive.js
+++ commandes/alive.js
@@ -43,15 +43,15 @@ const date = moment().format('DD/MM/YYYY');
  ${message}
  
  
- *ANYWAY-MD-WABOT*`
+ *HALLOVFEM-XMD-WABOT*`
 
  if (lien.match(/\.(mp4|gif)$/i)) {
     try {
         zk.sendMessage(dest, { video: { url: lien }, caption: alivemsg }, { quoted: ms });
     }
     catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
+        console.log("😁✌ Menu erreur " + e);
+        repondre("😁✌ Menu erreur " + e);
     }
 } 
 // Checking for .jpeg or .png
@@ -60,8 +60,8 @@ else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
         zk.sendMessage(dest, { image: { url: lien }, caption: alivemsg }, { quoted: ms });
     }
     catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
+        console.log("😁✌ Menu erreur " + e);
+        repondre("😁✌ Menu erreur " + e);
     }
 } 
 else {
